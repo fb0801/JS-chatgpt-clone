19615b6600d4865b2cdd3ab90cd99528c10ef44c
fb0801 <fbhatti0801@gmail.com>
2023-09-01 15:14:54 +0100

    api setup 2

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-const API_KEY = ''
+const API_KEY = 
+const submitButton = document.querySelector('#submit')
+
+async function getmessage(){
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json' 
+        }, 
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: 'Hello There!'}],
+            max_tokens: 100
+        })
+
+    }
+    try{
+        await fetch('https://api.openai.com/v1/chat/completions', options)
+
+    }catch{
+
+    }
+}
+
+
+
+
+submitButton.addEventListener('click', getmessage)
