4845c3ba5f75ff8a6d044d42857dcb70fe95d682
fb0801 <fbhatti0801@gmail.com>
2023-09-01 17:23:19 +0100

    save history and clear input

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const submitButton = document.querySelector('#submit')
 const outputElement = document.querySelector('#output')
 const inputElement = document.querySelector('input')
 const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value){
+    const inputElement = document.querySelector('input')
+    inputElement.value = value 
+}
 
 async function getmessage(){
     const options = {
@@ -23,9 +29,11 @@ async function getmessage(){
         const data = await respone.json()
         console.log(data)
         outputElement.textContent = data.choices[0].message.content
-        if(data.choices[0].message.content) {
+        if(data.choices[0].message.content ) {
             const pElement = document.createElement('p')
             pElement.textContent = inputElement.value 
+            pElement.addEventListener('click', () => changeInput(pElement.textContent))
+            historyElement.append(pElement)
         }
 
     }catch(error) {
@@ -37,3 +45,9 @@ async function getmessage(){
 
 
 submitButton.addEventListener('click', getmessage)
+
+function clearInput(){
+    inputElement.value = ''
+}
+
+buttonElement.addEventListener('click', clearInput)
